c457c7a016bd8b1b7fd8e1d67da161ce988f05eb
arikasoft <support@arikasoft.com>
2026-07-14 13:49:35 +0530

    Update route.ts

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,26 +1,10 @@
 import { NextResponse } from "next/server";
-import db from "@/lib/db";
 
 export async function GET() {
-  try {
-    const [rows] = await db.query("SELECT NOW() AS server_time");
-
-    return NextResponse.json({
-      success: true,
-      message: "Database Connected Successfully",
-      data: rows,
-    });
-  } catch (error) {
-    console.error("DB Error:", error);
-
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Database Connection Failed",
-        error:
-          error instanceof Error ? error.message : "Unknown error",
-      },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    DB_HOST: process.env.DB_HOST || "NOT FOUND",
+    DB_USER: process.env.DB_USER || "NOT FOUND",
+    DB_NAME: process.env.DB_NAME || "NOT FOUND",
+    DB_PORT: process.env.DB_PORT || "NOT FOUND",
+  });
 }
