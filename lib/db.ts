eec014d3b850b7b79d8964eda876aedf7a4cd7c7
arikasoft <support@arikasoft.com>
2026-07-14 14:49:16 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {},
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 export default db;
